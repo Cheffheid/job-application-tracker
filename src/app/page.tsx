4a45622b2524b5efc12838dc4466502959c1493f
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2024-12-09 17:09:01 -0500

    Replace mock data with data from the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,18 @@
-const mockData = [
-  {
-    company: "Automattic",
-    position: "Code Wrangler",
-    date: "11/22",
-    status: "pending",
-  },
-  {
-    company: "Happy Cog",
-    position: "Frontend Developer",
-    date: "11/22",
-    status: "pending",
-  },
-  {
-    company: "Tucows",
-    position: "Software Engineer - Frontend",
-    date: "11/22",
-    status: "pending",
-  },
-  {
-    company: "Carnegie",
-    position: "Frontend Developer",
-    date: "11/22",
-    status: "pending",
-  },
-];
+import { db } from "~/server/db";
 
-const mockApplications = mockData.map((application, index) => ({
-  id: index + 1,
-  ...application,
-}));
+export const dynamic = "force-dynamic";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const applications = await db.query.application.findMany();
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {mockApplications.map((application) => (
+        {applications.map((application) => (
           <div key={application.id} className="w-96 p-4">
-            <h2>{application.position}</h2>
+            <h2>{application.role}</h2>
             <p>
-              {application.company} &bull; {application.date} &bull;{" "}
+              {application.company} &bull; {application.appliedAt} &bull;{" "}
               {application.status}
             </p>
           </div>
