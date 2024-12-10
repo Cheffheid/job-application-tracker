fca5806ccbd4f4ef8c8a84f8d5b77fb2b357955e
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2024-12-10 15:05:20 -0500

    Only link pending applications

--- src/app/_components/applicationcard.tsx
+++ src/app/_components/applicationcard.tsx
@@ -15,16 +15,31 @@ export default function ApplicationCard({
   appliedAt,
   descriptionUrl,
 }: ApplicationCardProps) {
+  const content = (
+    <>
+      <h3 className="font-semibold">{role}</h3>
+      <p>{company}</p>
+      <p>
+        {appliedAt} &bull; {status}
+      </p>
+    </>
+  );
+
+  if ("rejected" === status) {
+    return (
+      <div className="w-full bg-gray-300 p-4 text-black shadow-sm">
+        {content}
+      </div>
+    );
+  }
+
   return (
     <Link
       href={descriptionUrl}
       target="_blank"
       className="w-full bg-white p-4 text-black shadow-sm duration-100 ease-in-out hover:bg-slate-700 hover:text-white"
     >
-      <h3 className="font-semibold">{role}</h3>
-      <p>
-        {company} &bull; {appliedAt} &bull; {status}
-      </p>
+      {content}
     </Link>
   );
 }
