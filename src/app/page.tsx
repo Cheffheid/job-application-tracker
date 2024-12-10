26111e54a2e68594939e193209f07aeba1bd8bb0
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2024-12-10 13:32:44 -0500

    Responsive updates to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ export default async function HomePage() {
   const applications = await db.query.application.findMany();
 
   return (
-    <div className="mx-auto max-w-7xl pt-4">
+    <div className="mx-auto w-full max-w-7xl px-4 pt-4">
       <h2 className="text-2xl font-semibold">Application List</h2>
-      <div className="grid grid-cols-4 gap-4 py-4">
+      <div className="grid gap-4 py-4 sm:grid-cols-2 lg:grid-cols-4">
         {applications.map((application) => (
           <div
             key={application.id}
