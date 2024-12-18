758e0bd90909c2bf05f8779a7590dc252b3965df
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2024-12-18 13:52:37 -0500

    Update modal with the update form

--- src/app/@modal/(.)application/update/[id]/page.tsx
+++ src/app/@modal/(.)application/update/[id]/page.tsx
@@ -1,4 +1,8 @@
+import UpdateForm from "~/app/_components/forms/update-form";
 import { Modal } from "./modal";
+import { db } from "~/server/db";
+import { eq } from "drizzle-orm";
+import { application } from "~/server/db/schema";
 
 export default async function ApplicationModal({
   params,
@@ -6,5 +10,27 @@ export default async function ApplicationModal({
   params: Promise<{ id: string }>;
 }) {
   const applicationId = (await params).id;
-  return <Modal>{applicationId}</Modal>;
+  const idAsNumber = Number(applicationId);
+
+  if (Number.isNaN(idAsNumber)) {
+    throw new Error("Invalid Application ID");
+  }
+
+  const applicationData = await db.query.application.findFirst({
+    columns: {
+      updatedAt: false,
+      createdAt: false,
+    },
+    where: eq(application.id, idAsNumber),
+  });
+
+  if (!applicationData) {
+    throw new Error("No Application Found With That ID");
+  }
+
+  return (
+    <Modal>
+      <UpdateForm application={applicationData} />
+    </Modal>
+  );
 }
