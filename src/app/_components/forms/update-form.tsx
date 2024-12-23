4732cd7836086f9521ed0a764750869c28d22a55
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2024-12-23 15:28:30 -0500

    Fix select label, add secondary status update

--- src/app/_components/forms/update-form.tsx
+++ src/app/_components/forms/update-form.tsx
@@ -3,7 +3,7 @@
 import React, { useActionState, useEffect, useState } from "react";
 import { toast } from "sonner";
 import { updateApplication } from "~/app/actions";
-import { statusesEnum } from "~/server/db/schema";
+import { statusesEnum, secondaryStatusesEnum } from "~/server/db/schema";
 
 export default function UpdateForm({
   application,
@@ -49,16 +49,20 @@ export default function UpdateForm({
   });
 
   const inputClasses =
-    "peer block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0";
-  const labelClasses =
-    "absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:text-gray-400 peer-focus:dark:text-blue-500";
+    "block w-full appearance-none border-0 border-b-2 border-gray-300 bg-transparent px-0 py-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0";
+  const labelClasses = "text-sm text-gray-500";
+
+  const floatingInputClasses = inputClasses + " peer";
+  const floatingLabelClasses =
+    labelClasses +
+    " absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4";
 
   return (
     <form className="min-w-96 bg-white p-4 text-black" action={formAction}>
       <div className="group relative z-0 mb-5 w-full">
         <input type="hidden" name="id" id="id" value={applicationData.id} />
         <input
-          className={inputClasses}
+          className={floatingInputClasses}
           type="text"
           name="role"
           id="role"
@@ -67,13 +71,13 @@ export default function UpdateForm({
           onChange={handleValueUpdate}
           required
         ></input>
-        <label className={labelClasses} htmlFor="role">
+        <label className={floatingLabelClasses} htmlFor="role">
           Role
         </label>
       </div>
       <div className="group relative z-0 mb-5 w-full">
         <input
-          className={inputClasses}
+          className={floatingInputClasses}
           type="text"
           name="company"
           id="company"
@@ -82,13 +86,13 @@ export default function UpdateForm({
           onChange={handleValueUpdate}
           required
         ></input>
-        <label className={labelClasses} htmlFor="role">
+        <label className={floatingLabelClasses} htmlFor="role">
           Company
         </label>
       </div>
       <div className="group relative z-0 mb-5 w-full">
         <input
-          className={inputClasses}
+          className={floatingInputClasses}
           type="date"
           name="applied_at"
           id="applied_at"
@@ -97,13 +101,13 @@ export default function UpdateForm({
           onChange={handleValueUpdate}
           required
         ></input>
-        <label className={labelClasses} htmlFor="role">
+        <label className={floatingLabelClasses} htmlFor="role">
           Application Date
         </label>
       </div>
       <div className="group relative z-0 mb-5 w-full">
         <input
-          className={inputClasses}
+          className={floatingInputClasses}
           type="url"
           name="descriptionurl"
           id="descriptionurl"
@@ -112,13 +116,13 @@ export default function UpdateForm({
           onChange={handleValueUpdate}
           required
         ></input>
-        <label className={labelClasses} htmlFor="role">
+        <label className={floatingLabelClasses} htmlFor="role">
           Job Description URL
         </label>
       </div>
       <div className="group relative z-0 mb-5 w-full">
         <input
-          className={inputClasses}
+          className={floatingInputClasses}
           type="url"
           name="statusurl"
           id="statusurl"
@@ -126,11 +130,14 @@ export default function UpdateForm({
           value={applicationData.statusUrl ? applicationData.statusUrl : ""}
           onChange={handleValueUpdate}
         ></input>
-        <label className={labelClasses} htmlFor="role">
+        <label className={floatingLabelClasses} htmlFor="role">
           Status URL
         </label>
       </div>
       <div className="group relative z-0 mb-5 w-full">
+        <label className={labelClasses} htmlFor="applicationStatus">
+          Status
+        </label>
         <select
           className={inputClasses}
           name="applicationStatus"
@@ -140,6 +147,25 @@ export default function UpdateForm({
           {statusesEnum.enumValues.map((statusText, idx) => {
             return (
               <option key={`status-${idx}`} value={statusText}>
+                {statusText}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <div className="group relative z-0 mb-5 w-full">
+        <label className={labelClasses} htmlFor="secondaryStatus">
+          Secondary Status
+        </label>
+        <select
+          className={inputClasses}
+          name="secondaryStatus"
+          id="secondaryStatus"
+          onChange={handleValueUpdate}
+        >
+          {secondaryStatusesEnum.enumValues.map((statusText, idx) => {
+            return (
+              <option key={`secondary-status-${idx}`} value={statusText}>
                 {statusText}
               </option>
             );
